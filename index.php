9591d695a58d25dd14f08a80b3497c677a8577ae
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-13 15:11:42 -0600

    Making progress!!

--- index.php
+++ index.php
@@ -19,9 +19,8 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
-		<!-- Lea's Custom CSS Below -->
-
-
+		<!-- Google Font -->
+		<link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
@@ -31,3 +30,45 @@
 
 		<title>Lea McDuffie Bootstrap Challenge</title>
 	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<!-- Header -->
+			<header>
+				<nav class="navbar navbar-default">
+					<div class="container-fluid">
+
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">littleloveprint</a>
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Navigation Station<span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="#">Do Something</a></li>
+										<li><a href="#">Do Something Else</a></li>
+										<li><a href="#">.. yeah</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="https://www.littleloveprint.io">Go to HERE!</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="box">
+								<h1>WELCOME!</h1>
+								<P>Hope you're well. I'm great. Let's go get food.</p>
+				</div>

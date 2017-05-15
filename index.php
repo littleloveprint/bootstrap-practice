f5effece24d195792e02d0215ff4e584d40127ae
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-14 22:29:04 -0600

    More progress. Not sure how to finish this without creating a CSS file.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 	<!-- Bootstrap Practice -->
 	<head>
 		<meta charset="utf-8"/>
@@ -7,10 +7,14 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Bootstrap Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+				crossorigin="anonymous"/>
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+				crossorigin="anonymous"/>
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -26,54 +30,99 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<title>Lea McDuffie Bootstrap Challenge</title>
 	</head>
+
 	<body class="sfooter">
 		<div class="sfooter-content">
 
 			<!-- Header -->
 			<header>
+
+				<!-- Navigation bar -->
 				<nav class="navbar navbar-default">
 					<div class="container-fluid">
 
 						<!-- Brand and toggle get grouped for better mobile display -->
 						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+									  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>
-							<nav class="navbar navbar-light bg-faded">
-								<a class="navbar-brand" href="#">
-									<img src="img/2.png" width="150" height="50" alt="littleloveprint logo">
-								</a>
+
+							<nav class="navbar-brand" href="#"><img src="img/2.png" width="150" height="50"
+																				 alt="littleloveprint logo" align="left"></a>
 							</nav>
 						</div>
+
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav navbar-right">
 								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Navigation Station<span class="caret"></span></a>
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+										aria-expanded="false">Navigation Station<span class="caret"></span></a>
 									<ul class="dropdown-menu">
-										<li><a href="#">Do Something</a></li>
-										<li><a href="#">Do Something Else</a></li>
-										<li><a href="#">.. yeah</a></li>
+										<li><a href="#">Bosque Brewing Co.</a></li>
+										<li><a href="#">Marble Brewery</a></li>
+										<li><a href="#">La Cumbre Brewing Co.</a></li>
 										<li role="separator" class="divider"></li>
-										<li><a href="https://www.littleloveprint.io">Go to HERE!</a></li>
+										<li><a href="https://www.littleloveprint.io">All Lea</a></li>
 									</ul>
 								</li>
 							</ul>
-						</div><!-- /.navbar-collapse -->
-					</div><!-- /.container-fluid -->
+						</div>
+						<!-- /.navbar-collapse -->
+					</div>
+					<!-- /.container-fluid -->
 				</nav>
+				<!-- /.navigation bar -->
+			</header>
+
+			<main>
 				<div class="container">
 					<div class="row">
 						<div class="col-xs-12">
 							<div class="box">
-									<h1 class="text-center">WELCOME!</h1>
-									<P class="text-center">Hope you're well. I'm great. Let's go get food.</p>
-								</box>
-				</div>
+								<h1 class="text-center">WELCOME!</h1>
+								<P class="text-center">Hope you're well. I'm great! Let's grab a beer!</p>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row featurette">
+							<div class="col-md-7 col-md-push-5">
+								<h2 class="featurette-heading">ABQrafty!</h2>
+								<p class="lead">Brought to us in 16-ounce pints on tap, the condensation-covered glass and dim
+									lighting yielded a
+									shimmering copper-colored brew with a tawny center and a slight touch of haze. Atop the brew,
+									a
+									wonderful tight lacing with more than ample head retention and stick. Aroma is big, with
+									notes of
+									resinous pine, grapefruit, distinct fruit cocktail characters and caramelized sugars spread
+									on
+									toasted bread.</p>
+							</div>
+							<div class="col-md-5 col-md-pull-7">
+								<img class="featurette-image img-responsive center-block" data-src="holder.js/400x400/auto"
+									  src="img/bosque-white-sands.gif" class="img-responsive" width="182.5" height="243.28"
+									  alt="Responsive image" data-holder-rendered="true">
+							</div>
+						</div>
+						<!-- /.row featurette -->
+						<hr class="featurette-dovider">
+					</div>
+			</main>
+		</div>
+		<!-- End of sfooter content -->
+		<footer>
+			<div class=""
+		</footer>
+	</body>
+</html>
